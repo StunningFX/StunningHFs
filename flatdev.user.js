f12428a8aa2041f47e708e00a88243ce2de0423a
StunningFX <stormasmr@gmail.com>
2016-03-15 18:51:11 +0000

    Update flatdev.user.js

--- flatdev.user.js
+++ flatdev.user.js
@@ -7,10 +7,10 @@
 // @include       http://hackforums.net/*
 // @author        iHydra/Kondax/Sasori
 // @updateURL     https://github.com/iHydra/flatdarkness/raw/master/flatdev.meta.js
-// @downloadURL   https://github.com/iHydra/flatdarkness/raw/master/flatdev.user.js
+// @downloadURL   https://github.com/StunningFX/StunningHFs/blob/master/flatdev.user.js
 // @require       https://code.jquery.com/jquery-2.1.4.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js
-// @resource      MainCSS https://github.com/iHydra/flatdarkness/raw/master/stylesheet_dev-15-309.css
+// @resource      MainCSS https://raw.githubusercontent.com/StunningFX/StunningHFs/master/stylesheet_1.5.5.css
 // @resource      HLCSS https://github.com/isagalaev/highlight.js/raw/master/src/styles/monokai_sublime.css 
 // @grant         GM_addStyle
 // @grant         GM_getResourceText
